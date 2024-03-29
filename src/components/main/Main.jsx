47a80da72986a7054044964bf669e293525b2b2c
Karim <karimmagdy096@gmail.com>
2024-03-29 12:46:31 +0200

    update

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 import "./main.css";
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <main className=" flex">
+      <section className=" flex   left-section">
+        <button className="active">All Projects</button>
+        <button>HTML&CSS</button>
+        <button>Java Script</button>
+        <button>React</button>
+      </section>
+      <section className="  right-section">
+        <article className="card border">
+          <img src="" alt="" />
+          <div className="box">
+            <h1 className="title">landing page 2</h1>
+            <p className="sub-title">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus minima laborum ullam.
+            </p>
+            <div className="flex">
+              <div className="icon-link"></div>
+              <div className="icon-github"></div>
+              <a href="">
+                more <span className="icon-arrow_forward"></span>
+              </a>
+            </div>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 }
