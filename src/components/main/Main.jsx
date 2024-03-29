2f1954265a01403df95873e0e098f26398bda3a0
Karim <karimmagdy096@gmail.com>
2024-03-29 16:22:35 +0200

    uPDATE

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,24 +9,28 @@ export default function Main() {
         <button>Java Script</button>
         <button>React</button>
       </section>
-      <section className="  right-section">
-        <article className="card border">
-          <img src="" alt="" />
-          <div className="box">
-            <h1 className="title">landing page 2</h1>
-            <p className="sub-title">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Necessitatibus minima laborum ullam.
-            </p>
-            <div className="flex">
-              <div className="icon-link"></div>
-              <div className="icon-github"></div>
-              <a href="">
-                more <span className="icon-arrow_forward"></span>
-              </a>
+      <section className="border flex  right-section">
+        {["aa", "bb", "cc"].map((item) => (
+          <article key={item} className="card ">
+            <img width={266} src="./test.png" alt="" />
+            <div style={{ width: "266px" }} className="box">
+              <h1 className="title">Landing page 2</h1>
+              <p className="sub-title">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Necessitatibus minima laborum ullam.
+              </p>
+              <div className="flex card-icons">
+                <div style={{ gap: "11px" }} className="flex">
+                  <div className="icon-link"></div>
+                  <div className="icon-github"></div>
+                </div>
+                <a className="card-link" href="">
+                  more <span className="icon-arrow_forward"></span>
+                </a>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
+        ))}
       </section>
     </main>
   );
