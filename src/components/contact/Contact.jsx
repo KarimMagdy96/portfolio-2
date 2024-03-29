85b4b76b12d507faf473b86a1625ed2adcd996ee
Karim <karimmagdy096@gmail.com>
2024-03-29 22:29:48 +0200

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,40 @@
 import React from "react";
 import "./contact.css";
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contact-us">
+      <h1 className="title">
+        <span className="icon-envelope"></span>
+        Contact Us
+      </h1>
+      <p className="sub-title">
+        Contact Us for more information and get notified when I publish
+        something new .
+      </p>
+      <div className="flex">
+        <form className="" action="">
+          <div className="flex">
+            <label htmlFor="Email">Email Address:</label>
+            <input
+              required
+              type="email"
+              id="email"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="flex" style={{ marginTop: "24px" }}>
+            <label htmlFor="message">Your Message:</label>
+            <textarea
+              placeholder="Massage"
+              required
+              name=""
+              id="message"
+            ></textarea>
+          </div>
+          <button className="submit">Sumbit</button>
+        </form>
+        <div className="animation border">animation</div>
+      </div>
+    </section>
+  );
 }
