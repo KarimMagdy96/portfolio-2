4a8a4bcb2838a99c4dfacdd4b51f3800f26ac434
Karim <karimmagdy096@gmail.com>
2024-03-29 08:25:44 +0200

    update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,19 +3,25 @@ import "./hero.css";
 export default function Hero() {
   return (
     <section className="hero flex">
-      <div className="left-section border">
+      <div className="left-section ">
         <div className="parent-avatar flex">
           <img className="avatar" src="./karim.png" alt="" />
           <div className="icon-verified"></div>
         </div>
         <h1 className="title">Front-End React Developer</h1>
-        <p>
+        <p className="subtitle">
           Versatile front-end developer with expertise in HTML5, CSS3, Sass,
           Bootstrap, jQuery, and JavaScript (ES6+), Proven track record in
           creating responsive web applications using React, Collaborative team
           player experienced in Git version control, Passionate about solving
           challenges and dedicated to delivering impactful user experiences.
         </p>
+        <div className="icons flex">
+          <div className="icon icon-twitter"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin"></div>
+        </div>
       </div>
       <div className="right-section animation border">
         Lorem ipsum dolor sit amet.
