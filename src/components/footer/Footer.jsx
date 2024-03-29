40502bc7b878a4b906a2b2eb6518893fd2646bf0
Karim <karimmagdy096@gmail.com>
2024-03-30 00:34:33 +0200

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 import "./footer.css";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex">
+      <ul className="flex">
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Projects</a>
+        </li>
+        <li>
+          <a href="">Speaking</a>
+        </li>
+        <li>
+          <a href="">User</a>
+        </li>
+      </ul>
+      <p>&copy;2024 Karim Magdy. All rights reserved.</p>
+    </footer>
+  );
 }
