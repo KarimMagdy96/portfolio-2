dfbad45234628d8e401a1d6c30a7c550ef628be8
Karim <karimmagdy096@gmail.com>
2024-04-04 15:22:06 +0200

    update

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,22 +3,41 @@ import "./main.css";
 import { AnimatePresence, motion } from "framer-motion";
 let myProjects = [
   {
-    projectTitle: "react Project",
-    ProjectDes: "lorem120",
-    category: "react",
-    projectImg: "a",
-  },
-  {
-    projectTitle: "Css Project",
-    ProjectDes: "lorem120",
-    category: "css",
-    projectImg: "b",
-  },
-  {
-    projectTitle: "javascript Project",
-    ProjectDes: "lorem120",
+    projectTitle: "RecipeRover-Food",
+    ProjectDes:
+      "RecipeRover Food Website Provides seamless intertwines coding prowess with the magic of culinary exploration",
     category: "javascript",
-    projectImg: "c",
+    projectUrl: "https://reciperover.vercel.app/",
+    projectGithub:
+      "https://github.com/KarimMagdy96/RecipeRover-Food?tab=readme-ov-file",
+    projectImg: "/public/project1.png",
+  },
+  {
+    projectTitle: "Gmate-Gym",
+    ProjectDes:
+      "Gymate provides a space for people to improve their physical fitness and overall health through regular exercise.",
+    category: "css",
+    projectUrl: "https://gymate-gym.vercel.app/",
+    projectGithub: "https://github.com/KarimMagdy96/Gmate-Gym",
+    projectImg: "/public/project2.png",
+  },
+  {
+    projectTitle: "MemoVerse",
+    ProjectDes:
+      "MemoVerse provides seamless idea capture with its user-friendly note app and real-time updates.",
+    category: "javascript",
+    projectUrl: "https://note-app-steel-theta.vercel.app/",
+    projectGithub: "https://github.com/KarimMagdy96/MemoVerse",
+    projectImg: "/public/project3.png",
+  },
+  {
+    projectTitle: "Movie-Wave",
+    ProjectDes:
+      "MovieWave is a website integrated with the latest movie API, providing access to the newest films and TV shows.",
+    category: "react",
+    projectUrl: "https://movie-wave-opal.vercel.app/#/login",
+    projectGithub: "https://github.com/KarimMagdy96/Movie-Wave",
+    projectImg: "/public/project4.png",
   },
 ];
 export default function Main() {
@@ -83,13 +102,22 @@ export default function Main() {
                 <p className="sub-title">{item.ProjectDes}</p>
                 <div className="flex card-icons">
                   <div style={{ gap: "11px" }} className="flex">
-                    <div className="icon-link"></div>
-                    <div className="icon-github"></div>
+                    <a
+                      href={item.projectUrl}
+                      target="_blank"
+                      className="icon-link"
+                    ></a>
+                    <a
+                      href={item.projectGithub}
+                      target="_blank"
+                      className="icon-github"
+                    ></a>
                   </div>
                   <a
                     className="card-link"
                     style={{ fontWeight: "bold" }}
-                    href=""
+                    target="_blank"
+                    href={item.projectUrl}
                   >
                     More <span className="icon-arrow_forward"></span>
                   </a>
